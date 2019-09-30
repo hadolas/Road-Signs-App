e01a5376d7361a1c7a98f02e8d3a2396edde5125
Hadolas <hadiyahmughal@gmail.com>
2019-09-30 11:41:16 +0100

    Make array of sign objects (test data)

---
+++ src/signs.js
@@ -0,0 +1,74 @@
+export const signs = [
+	{
+	 	id: 1,
+	 	img: 'maximum_speed.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 2,
+	 	img: 'no_buses(over 8 passenger seats).svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 3,
+	 	img: 'no_cycling.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 4,
+	 	img: 'no_entry.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 5,
+	 	img: 'no_left_turn.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 6,
+	 	img: 'no_motor_vehicles.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 7,
+	 	img: 'no_overtaking.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 8,
+	 	img: 'no_right_turn.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 9,
+	 	img: 'no_towed_caravans.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 10,
+	 	img: 'no_u_turn.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 11,
+	 	img: 'no_vehicles_carrying_explosives.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+	{
+	 	id: 12,
+	 	img: 'school_crossing_patrol.svg',
+	 	definition: 'Def',
+	 	type: 'Type'
+	},
+]
