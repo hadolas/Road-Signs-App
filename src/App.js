868fb9435a96061ac2831269faf75866e84970fa
Hadolas <hadiyahmughal@gmail.com>
2019-10-06 12:55:04 +0100

    Make SearchBox component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {signs} from './signs';
 
-const App = () => {
-	return (
-		<div>
-			<h1>Road Signs</h1>
-			<CardList signs={signs}/>
-		</div>
-	);
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			signs: signs,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value});
+	}
+
+
+	render() {
+		const filteredSigns = this.state.signs.filter(sign => {
+			return sign.img.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
+		return (
+			<div className='tc'>
+				<h1>Road Signs</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList signs={filteredSigns}/>
+			</div>
+		);
+	}
 }
 
 export default App;
